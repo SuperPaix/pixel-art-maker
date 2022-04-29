e0e37c86978f45041f4c7f19a340f7e015660564
SuperPaix <101859488+SuperPaix@users.noreply.github.com>
2022-04-29 14:32:00 +0100

    Add files via upload

--- project-pixel-art-maker-starter-master-peace/designs.js
+++ project-pixel-art-maker-starter-master-peace/designs.js
@@ -7,24 +7,24 @@ const submitButton = document.getElementById("submitBtn");
 const grid = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-submitButton.addEventListener('click' , function(x) { 
-    console.log("When the user clicks the submit button");
-    grid.innerHTML = " ";
-    x.preventDefault();
-    makeGrid();
+submitButton.addEventListener ('click', function(x){ 
+  console.log ("When the user clicks the submit button");
+  grid.innerHTML = " ";
+  x.preventDefault();
+  makeGrid();
 });
 //color selected cell
-grid.addEventListener('click', function(x) {
-    if(x.target.nodeName === 'TD') {
+grid.addEventListener ('click', function(x){
+  if (x.target.nodeName === 'TD') {
 		x.target.style.backgroundColor = gridColor.value; 
-		}
+	}
 });
 
 // Your code goes here!
-function makeGrid(x) {
-	for(var r = 0; r < gridHeight.value; r++){
+function makeGrid(x){
+	for (var r = 0; r < gridHeight.value; r++){
 		const row = grid.insertRow (0);
-		for( var c = 0; c < gridWidth.value; c++){
+		for (var c = 0; c < gridWidth.value; c++){
 			row.insertCell (0);
 		}
 	}
